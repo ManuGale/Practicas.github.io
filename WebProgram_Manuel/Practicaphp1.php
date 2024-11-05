abdb34b89757e09f010591fdd1083c6548afcb7c
Manuel GF <133926506+ManuGale@users.noreply.github.com>
2024-11-05 14:51:57 -0500

    commit php p1 por si acaso

---
+++ WebProgram_Manuel/Practicaphp1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> PHP feo wakala</title>
+</head>
+<body>
+    
+
+        <?php
+        echo "Hola! Scrip en php no es del diablo <br>";
+
+        // Comentario en linea 
+        # comentario normal en libxml_set_external_entity_loader
+
+        /* comentario de dos o mas lineas */
+
+
+        $vartext="Variables simples de php ";
+        echo $vartext;
+        $varnum1=5;
+        $varnum2=6;
+        $res=$varnum1 + $varnum2;
+        echo "resultado de la suma en php  wakala es de = $res " ;
+        
+        ?>
+    
+</body>
+</html>
